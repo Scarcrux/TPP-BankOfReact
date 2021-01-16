9b0b9525ff3cd31a90fcf1982df6c739d6228e7a
RyanGlem <60987736+RyanGlem@users.noreply.github.com>
2021-01-16 17:10:51 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,19 +11,53 @@ import Debits from './components/Debits';
 import axios from 'axios';
 
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
+  this.state = {
       accountBalance: 14568.27,
       currentUser: {
         userName: 'bob_loblaw',
         memberSince: '08/23/99',
+      },
+      error: null,
+      isLoaded: false,
+      creditsTotal: 0,
+      debitsTotal: 0,
+      credits: [],
+      debits: [],
+    }
+  }
 
-  mockLogIn = (logInInfo) => {
-    const newUser = {...this.state.currentUser}
-    newUser.userName = logInInfo.userName
-    this.setState({currentUser: newUser})
+  componentDidMount() {
+    Promise.all([
+      fetch(`https://moj-api.herokuapp.com/credits`).then(data => data.json()),
+      fetch(`https://moj-api.herokuapp.com/debits`).then(data => data.json())
+      ])
+      .then(
+        (data) => {
+          this.setState({
+            credits: data[0],
+            debits: data[1],
+            isLoaded: true
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+      .then((data) => {
+        let credits = this.state.credits.reduce((acc, item) => acc + item.amount, 0)
+        console.log(credits);
+        let debits = this.state.debits.reduce((acc, item) => acc + item.amount, 0)
+        console.log(debits)
+        this.setState({
+          creditsTotal: credits,
+          debitsTotal: debits,
+          accountBalance: credits - debits})
+        console.log(this.state.accountBalance)
+      }
+    )
   }
 
   render() {
